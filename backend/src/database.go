c9c21a1d41ff59c3ef9bab1739af50cbc8a0f138
Hudson Finn <hudson.finn@zohomail.com>
2026-03-14 12:08:47 -0400

    restart db stuff

--- backend/src/database.go
+++ backend/src/database.go
@@ -1,14 +1,4 @@
 package main
 
-import (
-	"database/sql"
-	"fmt"
-)
-
-func getAllRecipes() {
-	db, err := sql.Open("mysql", "admin:Mu$ici$c00l1@recipes")
-	if err != nil {
-		panic(fmt.Sprintf("SQL open error: %s", err.Error()))
-	}
-	db.Begin
+type Database struct {
 }
